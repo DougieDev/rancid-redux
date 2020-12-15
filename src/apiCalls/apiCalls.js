cf71cf816d4ed5860cd0fc66317c2158c2db2995
Robbie Jaeger <jaeger.rob@gmail.com>
2020-12-15 11:31:04 -0700

    Change to v2 of API

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://rancid-tomatillos.herokuapp.com/api/v1';
+const BASE_URL = 'https://rancid-tomatillos.herokuapp.com/api/v2';
 const MOVIES_ENDPOINT = '/movies/';
 const LOGIN_ENDPOINT = '/login/';
 const USERS = '/users/';
